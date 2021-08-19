ae1470db0cbdf069ba515c546b65c160b0e84a0d
JOSIAS GONÇALVES RODRIGUES <josiasgr@id.uff.br>
2021-08-18 21:53:47 -0300

    Primeiro commint

---
+++ src/main/java/br/com/senac/PrimeiraProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraProjetoApplication.class, args);
+	}
+
+}
